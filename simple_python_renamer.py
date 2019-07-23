0285dba6754c2f6b0475d302721dcb81d80969e9
salo <benjamin.cornwell96@gmail.com>
2019-07-22 20:27:27 -0400

    final committ

--- simple_python_renamer.py
+++ simple_python_renamer.py
@@ -6,11 +6,8 @@ __author__ = "Benjamin Cornwell"
 
 import os
 import os.path
-#import array as seasonRange
-#import array as filelist
 
 def main():
-
 
 	title = raw_input("Enter formatted title for show: ")
 	print("Title is: " + title)
@@ -22,10 +19,8 @@ def main():
 	for i in range(0,seasons):
 		srange = input("How many episodes are in season " + str(i+1) + "?: ")
 		seasonRange.append(srange)
-##	for y in seasonRange:
+
 	print(seasonRange)
-
-
 
 	list = os.listdir(filepath)
 	file_count = len(list)
@@ -41,18 +36,6 @@ def main():
 			print("DST :::" + dst)
 			os.rename(src, dst)
 			i=i+1
-
-
-#	list2 = os.listdir(filepath)
-#	print(list2)
-
-
-
-##	for filename in os.listdir("/media/sda/downloads/test"):
-##		dst = "Yu Yu Hakusho S" + "%02d" % x + "E" + "%02d" % i + ".mkv"
-##		os.rename(filename, dst)
-###		i += 1
-
 
 if __name__ == '__main__':
 	main()
