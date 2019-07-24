85e38fa75432e2054b02d9dd5cc4e45b736730fd
salo <benjamin.cornwell96@gmail.com>
2019-07-24 15:18:10 -0400

    refactored code, added documentation and comments

--- README.md
+++ README.md
@@ -3,17 +3,22 @@
 ## Simple Python Renamer
 
 
-A small python program that renames raw files to adhere to plex-compatible formatting. This short program can recieve raw, sorted data and format the names into media-server-compatible names.
+A small python program that renames raw files to adhere to [Plex-compatible formatting](https://support.plex.tv/articles/naming-and-organizing-your-tv-show-files/). This short program can recieve raw, sorted data and format the names into media-server-compatible names.
 
-Example of raw data:  "show_title x 0112 - episode.title.for.this.partituclar.episode (1999) (1080p)"
+Example of raw data: ```  "show_title x 0112 - episode.title.for.this.partituclar.episode (1999) (1080p).ext ```
 
-Example of reformatted data: "Show_title_S04E25"
+In the above example, Plex or Kodi would read 0112 as "Season 1, Episode 12", when it could really be the 112th episode of a series. The below format is what Plex says is the proper supported naming convention: 
 
-Titles in this notation are readable by popular home media server platforms like Plex and Kodi. 
+Example of reformatted data: ``` "ShowTitle - S04E25.ext" ```
 
-This Simple Python Renamer now has support for TV seasons with over 99 episodes in a single season. In such cases, the formatting with change from {01..99} to {001..999}.
+Adding a ``` (Year) ``` modifier is optional. The year distinction helps Plex determine which show to match to, if there exists another show or movie with a similar or identical name. 
+
+This Simple Python Renamer now has support for TV seasons with over 99 episodes in a single season. 
+
+The script may need to be made executable on your system. Do this with: ```sudo chmod +x simple_python_renamer ```
 
 To run: 
+ - Make sure that you meet the pre-conditions below. Call this program with ``` python3 simple_python_renamer ```
  - First, enter the name of the show in the form that as it should appear in proper format.
  - Second, enter the non-relative file-path. Using relative directory names like "." or "../" will work, but they are not recommended. 
  - Third, enter the amount of seasons
@@ -23,3 +28,5 @@ Pre-conditions:
  - Python3 is installed
  - The code is run on Linux or MacOS
  - The files are sorted in an ascending order. 01 - XX
+ - This code requres [natsort](https://github.com/SethMMorton/natsort) - which can be installed by ``` pip3 install natsort ```
+
--- simple_python_renamer.py
+++ simple_python_renamer.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 
+#TODO
+#add support for different video file types or different containers
+
+
+# According to Plex.tv's official support page, files that are listed for any television
+# viewing should be formatted in the following convention:
+# 		/TVShows/Showname (opt. Year)/ ShowName - S02E17.ext
+# source: https://support.plex.tv/articles/naming-and-organizing-your-tv-show-files/
+#This program will take raw, but numerically sorted data, and format that into place.
+#Formatting can use spaces, but other special characters are not recommended by Plex
+
 __author__ = "Benjamin Cornwell"
 
-#yu yu hakusho - s1 = 25  | s2 = 41 | s3 = 28 | s4 = 18
+# Requires pip3 install natsort - native Python sorting is not tenable for programs that
+# demand accuracy for file order.
 
 import os
 import errno
@@ -15,42 +27,55 @@ def main():
 	threeDigits = False
 	hasYear = False
 
-	filepath = input("Enter file path for data location - working directory can be relative - \".\"  : ")
+	print("Simple Python Renamer: A small and fast program that can rename your files to comply with Plex or Kodi naming requirements")
+
+	# Prompt user for input for construction
+	filepath = input("Enter file path for data location - working directory can be relative - \".\"   >> ")
 	if filepath == '.':
 		filepath = os.getcwd()
 	print("Working directory for Renamer: " + filepath)
-	title = input("Enter formatted title for show - ex. \"Tokyo_Ghoul\" : ")
+	title = input("Enter formatted title for show - ex. \"Tokyo Ghoul\"   >> ")
 
+	# natsort - 3rd party module - sorts list by [1,2,3..10,11..100,101]
+	# instead of [1,10,100,101,etc]
 	list = os.listdir(filepath)
 	fileCount = len(list)
 	list = natsorted(list, key=lambda y: y.lower())
-	print(list)
+	print("This is a list of all files in working directory: " + str(list))
 
+	# Support for Year - appears in title directory only
+	print("The (Year) indicator in the title is optional, and is only used to differentiate two shows of the same name")
+	print("Press [ENTER] to bypass (Year) designation and proceed to renaming")
 	try:
-		year = int(input("Optional: Enter the Year of production - ex. \"2008\" : "))
+		year = int(input("Optional: Enter the Year of production - ex. \"2008\"   >> "))
 	except ValueError:
 		year = None
+
 	print("making directory...")
+
+	# Constructing title directory - e.g. "Cops (1999)/"
+	# With or without year
 	if year != None:
-		hasYear = True 
-		titleYr = (title + "_(" + str(year) + ")")
+		hasYear = True
+		titleYr = (title + " (" + str(year) + ")")
 		print("Title is: " + titleYr)
 		os.mkdir(titleYr)
 		titleDir = titleYr
-		
 	else:
 		hasYear = False
 		print("Title is: " + title)
 		os.mkdir(title)
 		titleDir = title
 
-	seasons = int(input("How many seasons are you naming? : "))
+	seasons = int(input("How many seasons are you naming?   >> "))
 	print("Seasons num: " + str(seasons))
-	
+
+	# Create a directory for each season, to specify season separation.
+	# Changes working directory to the titleDir, makes the Season_i
+	#directories, and then changes back to the original working dir.
 	os.chdir(titleDir)
 	for i in range(0,seasons):
-		dirName = 'Season_{}'.format(i+1)
-		print("DIR DIR DIR DIR :::" + os.getcwd())
+		dirName = 'Season {}'.format(i+1)
 		try:
 			# Create target Directory
 			os.mkdir(dirName)
@@ -59,48 +84,41 @@ def main():
 			print("Directory " , dirName ,  " already exists")
 	os.chdir(filepath)
 
-	print("=============================== \n==============================")
-	print("Current working directory: " + os.getcwd())
-	print("Files in this directory: " + str(fileCount))
+	#dramatic diagnostics - mostly used for testing during writing
+	print("=+============================== \n==============================")
+	print("Working directory: " + os.getcwd())
+	print("Files in data directory: " + str(fileCount))
 	print("title directory: " + titleDir)
 	print("=============================== \n===============================")
 
-
-	print("This is the file list: ")
-	print(list)	
-
+	# Creates array of each season's episode count. If a single season of a show has
+	#more than 99 episodes, this will toggle a switch to reconfigure the file title
+	# - e.g. "Cops_S01E03.mkv" to be triple-count on E - e.g. "Cops_S01E104.mkv"
 	for i in range(0,seasons):
-		srange = int(input("How many episodes are in season " + str(i+1) + "?: "))
+		srange = int(input("How many episodes are in season " + str(i+1) + "?   >> "))
 		if srange > 99:
 			threedigits = True
 		seasonRange.append(srange)
 
-	print("Season Range: ")
-	print(seasonRange)
-
+	# i = epsiode count, or index of each file - before and after os.rename function
+	# Nested for loops allow processing of each Season directory first, then the drill
+	# down to depth=2 and rename and mv the files the their proper Season location
 	i=0
 	for x in range (0,seasons):
-		thisSeason = 'Season_{}'.format(x+1)
+		thisSeason = 'Season {}'.format(x+1)
 		workingDir = os.path.join(filepath,titleDir,thisSeason)
 		a = 0
-		print("WORKING DIR :::: ")
-		print(workingDir)
 		print("doing season " + str(x+1) + "...")
 		ind = seasonRange[x]
 		for y in range (ind):
-
-			newName = title + "_S" + "%02d" % (x+1) + "E" + "%02d" % (y+1) + ".mkv"
 			if threeDigits == True:
-				dst = os.path.join(filepath,'Season_{}'.format(x+1),title + "_S" + "%02d" % (x+1) + "E" + "%03d" % (y+1) + ".mkv")
+				newName = title + " - S" + "%02d" % (x+1) + "E" + "%03d" % (y+1) + ".mkv"
 			else:
-				dst = os.path.join(workingDir,newName)
-			
+				newName = title + " - S" + "%02d" % (x+1) + "E" + "%02d" % (y+1) + ".mkv"
+			dst = os.path.join(workingDir,newName)
 			src = os.path.join(filepath, list[i])
-			print("DST ::: " + dst)
-			print("SRC ::: " + src)
-			print("list[i]: " + list[i])
 			os.rename(src, dst)
-			print("new file: " + list[i])
+			print("new file: " + src)
 			i=i+1
 
 if __name__ == '__main__':
